08e69046d3ec7a135c3d03d6a977ec15d7ef64a5
Bogdan Onischenko <dever4eg@gmail.com>
2021-05-05 20:51:08 +0300

    Separated rendering and recognition to different effects

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,6 +3,7 @@ import {usePoseNet} from "../hooks/usePoseNet";
 import {VideoContext} from "./Camera";
 import {drawKeypoints, drawSkeleton, drawVideo} from "../helpers/draw.js";
 import {SettingsContext} from "./SessingsContextPovider";
+import AsyncLock from 'async-lock'
 
 function Preview (props) {
     const { width = 600, height = 600 } = props
@@ -14,42 +15,58 @@ function Preview (props) {
     const minPoseConfidence = 0.1
     const minPartConfidence = 0.5
 
+    const lock = useRef(new AsyncLock())
+
+    let poses = []
+
+    const draw = (ctx) => {
+        ctx.clearRect(0, 0, width, height);
+
+        if (videoIsLoaded) {
+            drawVideo(ctx, video, width, height)
+        }
+
+        poses.forEach(({score, keypoints}) => {
+            if (score >= minPoseConfidence) {
+                drawSkeleton(keypoints, minPartConfidence, ctx);
+                drawKeypoints(keypoints, minPartConfidence, ctx)
+            }
+        });
+    }
+
     useEffect(() => {
         const ctx = canvasElement.current.getContext('2d');
 
         let shouldRender = true
         const startDetection = async () => {
-            if (!shouldRender) {
-                return
-            }
-
-            let poses = []
-            if (videoIsLoaded && !modelIsLoading) {
-                poses = await estimatePoses(video)
-            }
-
-            ctx.clearRect(0, 0, width, height);
-
-            if (videoIsLoaded) {
-                drawVideo(ctx, video, width, height)
-            }
-
-            poses.forEach(({score, keypoints}) => {
-                if (score >= minPoseConfidence) {
-                    drawSkeleton(keypoints, minPartConfidence, ctx);
-                    drawKeypoints(keypoints, minPartConfidence, ctx)
-                }
-            });
-
+            if (!shouldRender) return
+            draw(ctx)
             requestAnimationFrame(startDetection)
         }
-
         startDetection()
 
         return () => {
             shouldRender = false
         }
     }, [modelIsLoading, videoIsLoaded, width, height])
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (lock.current.isBusy()) {
+                return
+            }
+
+            lock.current.acquire('recognition', async () => {
+                if (videoIsLoaded && !modelIsLoading) {
+                    poses = await estimatePoses(video)
+                }
+            }, null, {})
+        }, 50)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [modelIsLoading, videoIsLoaded])
 
     return (
         <div>
